d5d00240da6f8546ae9cd60ba7d079dffe9c8fe3
xb.wang <xb.wang@rock-chips.com>
2022-01-20 11:18:07 +0800

    build: export head files to out dir for linux
    Signed-off-by: xb.wang <xb.wang@rock-chips.com>
    Change-Id: Iea1599cdc19e3fca82abefab24e0b2b5d8c045fa

--- build.sh
+++ build.sh
@@ -4,19 +4,30 @@ set -e
 
 collect_bin()
 {
-	cp $BUILD_DIR/librkcrypto.so $TARGET_DIR
-	cp $BUILD_DIR/librkcrypto.a $TARGET_DIR
-	cp $BUILD_DIR/test/librkcrypto_test $TARGET_DIR
-	cp $BUILD_DIR/demo/librkcrypto_demo $TARGET_DIR
-	echo "copy target files to $TARGET_DIR success"
+	cp $BUILD_DIR/librkcrypto.so $TARGET_LIB_DIR
+	cp $BUILD_DIR/librkcrypto.a $TARGET_LIB_DIR
+	cp $BUILD_DIR/test/librkcrypto_test $TARGET_BIN_DIR
+	cp $BUILD_DIR/demo/librkcrypto_demo $TARGET_BIN_DIR
+	echo "copy target files to $TARGET_LIB_DIR success"
+
+	# export head files
+	cp $SCRIPT_DIR/include/rkcrypto_otp_key.h $TARGET_INCLUDE_DIR
+	cp $SCRIPT_DIR/include/rkcrypto_common.h $TARGET_INCLUDE_DIR
+	cp $SCRIPT_DIR/include/rkcrypto_core.h $TARGET_INCLUDE_DIR
+	cp $SCRIPT_DIR/include/rkcrypto_mem.h $TARGET_INCLUDE_DIR
+	echo "copy head files to $TARGET_INCLUDE_DIR success"
 }
 
 build()
 {
 	echo "build $ARM_BIT libraries and binaries"
-	TARGET_DIR=$SCRIPT_DIR/out/target/$ARM_BIT/
+	TARGET_LIB_DIR=$SCRIPT_DIR/out/target/lib/$ARM_BIT/
+	TARGET_BIN_DIR=$SCRIPT_DIR/out/target/bin/$ARM_BIT/
+	TARGET_INCLUDE_DIR=$SCRIPT_DIR/out/target/include/
 	BUILD_DIR=$SCRIPT_DIR/out/build/$ARM_BIT/
-	mkdir -p $TARGET_DIR
+	mkdir -p $TARGET_LIB_DIR
+	mkdir -p $TARGET_BIN_DIR
+	mkdir -p $TARGET_INCLUDE_DIR
 	mkdir -p $BUILD_DIR
 	cd $BUILD_DIR
 	cmake $SCRIPT_DIR $DBUILD
