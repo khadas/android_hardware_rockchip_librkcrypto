fb42e22fa3b9d71a10fd9f074e49d65e1b526fbd
Lin Jinhan <troy.lin@rock-chips.com>
2022-03-23 10:05:59 +0800

    src: mem: modify to mem ops
    Signed-off-by: Lin Jinhan <troy.lin@rock-chips.com>
    Change-Id: Ic6d8598f6a57b307481fa2cc6e50f64a8660716e

--- src/rkcrypto_mem.c
+++ src/rkcrypto_mem.c
@@ -17,10 +17,14 @@
 #include "rkcrypto_trace.h"
 #include "rk_list.h"
 
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#endif
+
 #define DRM_MODULE_NAME "rockchip"
 #define DRM_CARD_PATH "/dev/dri/card0"
 
-#define IS_DRM_INVALID()	(drm_fd < 0)
+#define IS_DMA_INVALID()	(dma_node_fd < 0)
 
 struct mem_pool_node {
 	rk_crypto_mem		mem;
@@ -29,12 +33,37 @@ struct mem_pool_node {
 	struct list_head	list;
 };
 
-static int drm_fd = -1;
-static struct list_head mem_pool_list;
-pthread_mutex_t drm_mutex = PTHREAD_MUTEX_INITIALIZER;
-static int mem_init_cnt;
+struct mem_ops {
+	int (*init)(void);
+	void (*deinit)(int dma_node_fd);
+	struct mem_pool_node *(*alloc_node)(int dma_node_fd, uint32_t size);
+	void (*free_node)(int dma_node_fd, struct mem_pool_node *node);
+};
 
-static struct mem_pool_node *crypto_alloc_node(uint32_t size)
+static int dma_node_fd = -1;
+static struct list_head mem_pool_list;
+pthread_mutex_t dma_mutex = PTHREAD_MUTEX_INITIALIZER;
+static int mem_init_cnt;
+struct mem_ops *cur_mem_pos;
+
+static int crypto_init_drm(void)
+{
+	int fd;
+
+	fd = open(DRM_CARD_PATH, O_RDWR);
+	if (fd < 0)
+		D_TRACE("failed to open drm !\n");
+
+	return fd;
+}
+
+static void crypto_deinit_drm(int dma_node_fd)
+{
+	if (dma_node_fd >= 0)
+		close(dma_node_fd);
+}
+
+static struct mem_pool_node *crypto_alloc_node_drm(int dma_node_fd, uint32_t size)
 {
 	int ret = -1;
 	size_t min_size;
@@ -56,20 +85,20 @@ static struct mem_pool_node *crypto_alloc_node(uint32_t size)
 	memset(node, 0x00, sizeof(*node));
 	memset(&map_req, 0x00, sizeof(map_req));
 
-	ret = drmIoctl(drm_fd, DRM_IOCTL_ROCKCHIP_GEM_CREATE, &req);
+	ret = drmIoctl(dma_node_fd, DRM_IOCTL_ROCKCHIP_GEM_CREATE, &req);
 	if (ret) {
 		free(node);
 		return NULL;
 	}
 
-	ret = drmPrimeHandleToFD(drm_fd, req.handle, 0, &node->mem.dma_fd);
+	ret = drmPrimeHandleToFD(dma_node_fd, req.handle, 0, &node->mem.dma_fd);
 	if (ret) {
-		E_TRACE("failed to get dma fd.\n");
+		E_TRACE("failed to get dma dma_node_fd.\n");
 		goto error;
 	}
 
 	map_req.handle = req.handle;
-	ret = drmIoctl(drm_fd, DRM_IOCTL_ROCKCHIP_GEM_MAP_OFFSET, &map_req);
+	ret = drmIoctl(dma_node_fd, DRM_IOCTL_ROCKCHIP_GEM_MAP_OFFSET, &map_req);
 	if (ret) {
 		E_TRACE("failed to ioctl gem map offset.");
 		goto error;
@@ -80,10 +109,10 @@ static struct mem_pool_node *crypto_alloc_node(uint32_t size)
 
 #ifdef __ANDROID__
 	node->mem.vaddr = mmap64(0, req.size, PROT_READ | PROT_WRITE, MAP_SHARED,
-				 drm_fd, map_req.offset);
+				 dma_node_fd, map_req.offset);
 #else
 	node->mem.vaddr = mmap(0, req.size, PROT_READ | PROT_WRITE, MAP_SHARED,
-				 drm_fd, map_req.offset);
+				 dma_node_fd, map_req.offset);
 #endif
 	if (node->mem.vaddr == MAP_FAILED) {
 		E_TRACE("failed to mmap buffer. offset = %"PRIu64", reason: %s\n",
@@ -98,7 +127,7 @@ static struct mem_pool_node *crypto_alloc_node(uint32_t size)
 
 	return node;
 error:
-	drmIoctl(drm_fd, DRM_IOCTL_GEM_CLOSE, &req);
+	drmIoctl(dma_node_fd, DRM_IOCTL_GEM_CLOSE, &req);
 
 	if (node)
 		free(node);
@@ -106,7 +135,7 @@ error:
 	return NULL;
 }
 
-static void crypto_free_node(struct mem_pool_node *node)
+static void crypto_free_node_drm(int dma_node_fd, struct mem_pool_node *node)
 {
 	struct drm_gem_close req;
 	size_t min_size;
@@ -125,35 +154,52 @@ static void crypto_free_node(struct mem_pool_node *node)
 	if (node->mem.dma_fd >= 0)
 		close(node->mem.dma_fd);
 
-	drmIoctl(drm_fd, DRM_IOCTL_GEM_CLOSE, &req);
+	drmIoctl(dma_node_fd, DRM_IOCTL_GEM_CLOSE, &req);
 
 	free(node);
 }
 
+struct mem_ops rk_mem_ops_tbl[] = {
+	{
+		.init = crypto_init_drm,
+		.deinit = crypto_deinit_drm,
+		.alloc_node = crypto_alloc_node_drm,
+		.free_node  = crypto_free_node_drm,
+	},
+};
+
 int rk_crypto_mem_init(void)
 {
 	int ret = -1;
+	uint32_t i;
 
 	if (mem_init_cnt > 0) {
 		ret = 0;
-		goto exit;
+		return ret;
 	}
 
-	pthread_mutex_lock(&drm_mutex);
+	pthread_mutex_lock(&dma_mutex);
 
 	INIT_LIST_HEAD(&mem_pool_list);
 
-	drm_fd = open(DRM_CARD_PATH, O_RDWR);
-	if (drm_fd < 0) {
-		E_TRACE("failed to open drm !\n");
+	for (i = 0; i < ARRAY_SIZE(rk_mem_ops_tbl); i++) {
+		dma_node_fd = rk_mem_ops_tbl[i].init();
+		if (dma_node_fd >= 0)
+			break;
+	}
+
+	if (dma_node_fd < 0) {
+		ret = -1;
 		goto exit;
 	}
+
+	cur_mem_pos = &rk_mem_ops_tbl[i];
 
 	mem_init_cnt++;
 
 	ret = 0;
 exit:
-	pthread_mutex_unlock(&drm_mutex);
+	pthread_mutex_unlock(&dma_mutex);
 
 	return ret;
 }
@@ -164,47 +210,49 @@ void rk_crypto_mem_deinit(void)
 	struct mem_pool_node *node;
 	struct list_head *pos, *n;
 
-	pthread_mutex_lock(&drm_mutex);
+	pthread_mutex_lock(&dma_mutex);
 
 	mem_init_cnt--;
 	if (mem_init_cnt > 0)
 		goto exit;
 
-	if (IS_DRM_INVALID())
+	if (IS_DMA_INVALID())
 		goto exit;
 
 	list_for_each_safe(pos, n, &mem_pool_list) {
 		node = list_entry(pos, struct mem_pool_node, list);
 		list_del(pos);
-		crypto_free_node(node);
+
+		if (cur_mem_pos)
+			cur_mem_pos->free_node(dma_node_fd, node);
 	}
 
-	if (drm_fd >= 0)
-		close(drm_fd);
+	if (cur_mem_pos)
+		cur_mem_pos->deinit(dma_node_fd);
 exit:
-	pthread_mutex_unlock(&drm_mutex);
+	pthread_mutex_unlock(&dma_mutex);
 }
 
 rk_crypto_mem *rk_crypto_mem_alloc(size_t size)
 {
 	struct mem_pool_node *node;
 
-	pthread_mutex_lock(&drm_mutex);
+	pthread_mutex_lock(&dma_mutex);
 
-	if (IS_DRM_INVALID())
+	if (IS_DMA_INVALID() || !cur_mem_pos)
 		goto error;
 
-	node = crypto_alloc_node(size);
+	node = cur_mem_pos->alloc_node(dma_node_fd, size);
 	if (!node)
 		goto error;
 
 	list_add_tail(&node->list, &mem_pool_list);
 
-	pthread_mutex_unlock(&drm_mutex);
+	pthread_mutex_unlock(&dma_mutex);
 
 	return &node->mem;
 error:
-	pthread_mutex_unlock(&drm_mutex);
+	pthread_mutex_unlock(&dma_mutex);
 
 	return NULL;
 }
@@ -214,9 +262,9 @@ void rk_crypto_mem_free(rk_crypto_mem *memory)
 	struct mem_pool_node *node;
 	struct list_head *pos, *n;
 
-	pthread_mutex_lock(&drm_mutex);
+	pthread_mutex_lock(&dma_mutex);
 
-	if (IS_DRM_INVALID())
+	if (IS_DMA_INVALID() || !cur_mem_pos)
 		goto exit;
 
 	if (!memory)
@@ -227,11 +275,11 @@ void rk_crypto_mem_free(rk_crypto_mem *memory)
 
 		if (&node->mem == memory) {
 			list_del(pos);
-			crypto_free_node(node);
+			cur_mem_pos->free_node(dma_node_fd, node);
 			goto exit;
 		}
 	}
 
 exit:
-	pthread_mutex_unlock(&drm_mutex);
+	pthread_mutex_unlock(&dma_mutex);
 }
